06f59798cba110d78b8b77a6768c843466f676bf
Shaun Yong <shaunyong9@gmail.com>
2018-11-01 19:56:40 +0800

    first commit

---
+++ server_1.js
@@ -0,0 +1,12 @@
+const app = require('http')
+   .createServer((req, res)=> res.send('oh hi there!'));
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+console.log(process.env);
+
+
+
